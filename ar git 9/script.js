a07ee94aa6e2522356b2803203e34350715bfb81
arbishghori <128428077+arbishghori@users.noreply.github.com>
2023-08-12 09:56:15 -0700

    Add files via upload

---
+++ ar git 9/script.js
@@ -0,0 +1,23 @@
+ var integerString = "24"
+ var num = Number(integerString);
+console.log(integerString);
+
+// we can also use alert instead of console.log
+//just like this example
+ alert(integerString)
+
+ var floatingNumString = "24.9876";
+ var num = Number(floatingNumString);
+ console.log(floatingNumString);
+// we can also use alert instead of console.log
+// just like this example
+alert (floatingNumString)
+
+
+ var numberAsNumber = 1234;
+ var numberAsString = numberAsNumber.toString();
+ console.log(numberAsNumber.toString());
+
+ // we can also use alert instead of console.log
+ //just like this example
+ alert(numberAsNumber.toString())
